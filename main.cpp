12066ab978e011cd418d6c67a347991d733a21d1
Z1R0Axcel <113200832+Z1R0Axcel@users.noreply.github.com>
2022-11-17 10:53:27 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//J00916724 Luke McClinton 11/16/2022 Page 278
+// J00916724 Luke McClinton 11/16/2022 Page 278
 #include <iomanip>                               //Line 3
 #include <iostream>                              //Line 1
 #include <string>                                //Line 2
@@ -12,6 +12,8 @@ int main()                                       // Line 6
   int totalNumOfBoxesSold;                       // Line 11
   double costOfOneBox;                           // Line 12
   cout << fixed << showpoint << setprecision(2); // Line 13
+  cout << "Line 14: Enter each volunteer's name and the number of boxes sold "
+          "by each volunteer, ending with -1: "
        << endl;                                                 // Line 14
   totalNumOfBoxesSold = 0;                                      // Line 15
   numOfVolunteers = 0;                                          // Line 16
@@ -31,10 +33,12 @@ int main()                                       // Line 6
   cout << endl;                                   // Line 29
   cout << "Line 30: The total money made by selling cookies: $"
        << totalNumOfBoxesSold * costOfOneBox << endl; // Line 30
-  if (numOfVolunteers != 0)                           // Line 31
+  if (numOfVolunteers != 0) {                         // Line 31
     cout << "The average number of boxes sold by each volunteer: "
-         << totalNumOfBoxesSold / numOfVolunteers << endl; // Line 32
-  else                                                     // Line 33
-    cout << "No input." << endl;                           // Line 34
-  return 0;                                                // Line 35
+         << totalNumOfBoxesSold / numOfVolunteers << endl;
+  }                              // Line 32
+  else                           // Line 33
+    cout << "No input." << endl; // Line 34
+  return 0;                      // Line 35
 }
+
